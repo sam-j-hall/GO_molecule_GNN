5c634e15e0ba90ae93b06f29fba7678414002576
Sam Hall <samuel.hall@helmholtz-berlin.de>
2023-11-03 13:15:50 +0100

    Update

--- GNN_atom.py
+++ GNN_atom.py
@@ -61,7 +61,7 @@ class GNN(torch.nn.Module):
 #             torch.nn.Linear(self.out_channels[-1], self.num_tasks))
             
             self.graph_pred_linear = torch.nn.Linear(self.out_channels[-1], self.num_tasks)
-            #self.graph_pred_linear1 = torch.nn.Linear(200, 200)
+            self.graph_pred_linear1 = torch.nn.Linear(200, 200)
 	
 
     def forward(self, batched_data):
@@ -70,12 +70,12 @@ class GNN(torch.nn.Module):
 
         #ic(batched_data.batch.shape)
         #ic(batched_data.atom_num)
-        atom = batched_data.atom_num
+        #atom = batched_data.atom_num
         #ic(h_node[atom])
         #ic(h_node)
 
-        h_graph = h_node[atom]
-        #h_graph = self.pool(h_node, batched_data.batch)
+        #h_graph = h_node[atom]
+        h_graph = self.pool(h_node, batched_data.batch)
 
         #ic(h_graph.shape)
         #ic(h_graph)
@@ -98,7 +98,7 @@ class GNN(torch.nn.Module):
    
         out = p(self.graph_pred_linear(h_graph))
 
-        #out = p(self.graph_pred_linear1(out))
+        out = p(self.graph_pred_linear1(out))
 
         return  out #, h_select, h_weight, h_out
     
