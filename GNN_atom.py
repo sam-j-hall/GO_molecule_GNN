1c129824fca2d983a7196955e606e99de5fc72c5
Sam Hall <samuel.hall@helmholtz-berlin.com>
2024-02-18 17:59:13 +0100

    New atom testing

--- GNN_atom.py
+++ GNN_atom.py
@@ -61,7 +61,7 @@ class GNN(torch.nn.Module):
 #             torch.nn.Linear(self.out_channels[-1], self.num_tasks))
             
             self.graph_pred_linear = torch.nn.Linear(self.out_channels[-1], self.num_tasks)
-            #self.graph_pred_linear1 = torch.nn.Linear(200, 200)
+            self.graph_pred_linear1 = torch.nn.Linear(200, 200)
 	
 
     def forward(self, batched_data):
@@ -203,7 +203,7 @@ class GNN_node(torch.nn.Module):
 
         for layer in range(self.num_layer):
 
-            h = self.convs[layer](h_list[layer], edge_index)
+            h = self.convs[layer](h_list[layer], edge_index, edge_attr=edge_attr)
 
             h = F.relu(h)
    
